665c376a2f2eb4bbe938d061dfafd234fe1b2551
Sebastian Wallin <sebastian.wallin@gmail.com>
2011-10-03 19:31:42 +0200

    Make reusable

--- js/application.js
+++ js/application.js
@@ -1,6 +1,5 @@
 $.fn.reverse = [].reverse;
 
-var NUM_COLUMNS = 3;
 var NUM_PRODUCTS = 9;
 var SWIPE_IN_DELAY = 200;
 var SWIPE_OUT_DELAY = 150;
@@ -10,32 +9,42 @@ var tmpl = {
 };
 
 
-var Swipe = {
-  el: null,
+var Swipe = function (opts) {
+  opts = opts || {};
+  this.el = opts.el;
+  this.inClass = opts.inClass || 'out-left';
+  this.outClass = opts.outClass || 'out-right';
+  this.onAdd = opts.onAdd || function () {};
+  this.numCols = opts.numCols || this.NUM_COLUMNS;
+  _.bindAll(this, 'enter', 'leave');
+};
 
-  inClass: 'out-left',
-  outClass: 'out-right',
-
-  enter: function () {
+Swipe.prototype = {
+  NUM_COLUMNS: 4,
+  enter: function (data) {
+    var self = this;
     // Remove any existing columns
-    var $existing = $('ul.col');
+    var $existing = $('ul.col', this.el);
     if ($existing.length > 0) {
-      Swipe.leave();
-      _.delay(Swipe.enter, NUM_COLUMNS * SWIPE_OUT_DELAY);
+      this.leave();
+      _.delay(function () {
+        self.enter(data);
+      }, this.numCols * SWIPE_OUT_DELAY);
       return;
     }
 
     // Add fresh columns
     var $cols = [];
-    for (var i = 0; i < NUM_COLUMNS; i++) {
-      $cols[i] = $('<ul>').attr('class', 'col ' + Swipe.inClass);
-      Swipe.el.append($cols[i]);
+    for (var i = 0; i < this.numCols; i++) {
+      $cols[i] = $('<ul>').attr('class', 'col ' + this.inClass);
+      this.el.append($cols[i]);
     }
 
-    // Add products
-    for (i = 0; i < NUM_PRODUCTS; i++) {
-      var $col = $cols[i % NUM_COLUMNS];
-      $col.append($(tmpl.item));
+    // Add data
+    for (i = 0, len = data.length; i < len; i++) {
+      var item = data[i];
+      var $col = $cols[i % this.numCols];
+      $col.append(this.onAdd(item, i));
     }
 
     var len = $cols.length;
@@ -47,14 +56,14 @@ var Swipe = {
     });
   },
 
-
   leave: function () {
-    var $cols = $('ul.col');
+    var self = this;
+    var $cols = $('ul.col', this.el);
     var len = $cols.length;
     var time = 0;
     $cols.each(function (i, el) {
       _.delay(function () {
-        $(el).addClass(Swipe.outClass);
+        $(el).addClass(self.outClass);
       }, time += SWIPE_OUT_DELAY);
     });
 
@@ -66,14 +75,21 @@ var Swipe = {
 };
 
 $(function () {
-  Swipe.el = $('#wrapper');
+  swipe = new Swipe({
+    el: $('#wrapper'),
+    onAdd: function (item) {
+      return $(tmpl.item);
+    }
+  });
+  data = new Array(NUM_PRODUCTS);
+  $('#swipe-in').click(function () {
+    swipe.enter(data);
+  });
 
-  $('#swipe-in').click(Swipe.enter);
+  $('#swipe-out').click(swipe.leave);
 
-  $('#swipe-out').click(Swipe.leave);
-
-  $('#effect').change(function(e) {
-    Swipe.outClass = $(e.target).val();
+  $('#effect').change(function (e) {
+    swipe.outClass = $(e.target).val();
   });
 });
 
