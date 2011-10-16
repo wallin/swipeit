4f0ff52eec99ccd45c9c45530473d83283ece016
Sebastian Wallin <sebastian.wallin@gmail.com>
2011-10-16 17:58:43 +0200

    Make bi-directional

--- js/application.js
+++ js/application.js
@@ -6,12 +6,11 @@ var tmpl = {
   item: '<li><img src="img/product.jpg"></li>'
 };
 
-
 var Swipe = function (opts) {
   opts = opts || {};
   this.el = opts.el;
-  this.inClass = opts.inClass || 'out-left';
-  this.outClass = opts.outClass || 'out-right';
+  this.inClass = opts.inClass || 'in';
+  this.outClass = opts.outClass || 'out';
   this.onAdd = opts.onAdd || function () {};
   this.numCols = opts.numCols || this.NUM_COLUMNS;
   _.bindAll(this, 'enter', 'leave');
@@ -21,22 +20,29 @@ Swipe.prototype = {
   NUM_COLUMNS: 4,
   SWIPE_IN_DELAY: 200,
   SWIPE_OUT_DELAY: 150,
-  enter: function (data) {
+  enter: function (data, direction) {
+    if (data == null || (_.isArray(data) && data.length === 0)) {
+      return;
+    }
+    if (direction !== 'left') {
+      direction = 'right';
+    }
+
     var self = this;
     // Remove any existing columns
     var $existing = $('ul.col', this.el);
-    if ($existing.length > 0) {
-      this.leave();
+    if ($existing.length > 0 && !$existing.hasClass('deleted')) {
+      this.leave(direction);
       _.delay(function () {
-        self.enter(data);
-      }, this.numCols * this.SWIPE_OUT_DELAY);
+        self.enter(data, direction);
+      }, this.SWIPE_OUT_DELAY);
       return;
     }
 
     // Add fresh columns
     var $cols = [];
     for (var i = 0; i < this.numCols; i++) {
-      $cols[i] = $('<ul>').attr('class', 'col ' + this.inClass);
+      $cols[i] = $('<ul>').attr('class', 'col ' + this.inClass + '-' + direction);
       this.el.append($cols[i]);
     }
 
@@ -48,23 +54,28 @@ Swipe.prototype = {
     }
 
     var len = $cols.length;
-    var time = this.SWIPE_IN_DELAY * this.numCols;
+    var time = direction === 'right' ? this.SWIPE_IN_DELAY * this.numCols : 0;
+    var mult = direction === 'right' ? -1 : 1;
     _.each($cols, function (el, i) {
       _.delay(function () {
         el.attr('class', 'col col' + (i + 1));
-      }, time -= self.SWIPE_IN_DELAY);
+      }, time += self.SWIPE_IN_DELAY * mult);
     });
   },
 
-  leave: function () {
+  leave: function (direction) {
+    if (direction !== 'left') {
+      direction = 'right';
+    }
     var self = this;
-    var $cols = $('ul.col', this.el);
+    var $cols = $('ul.col', this.el).addClass('deleted');
     var len = $cols.length;
-    var time = this.SWIPE_OUT_DELAY * this.numCols;
+    var time = direction === 'right' ? this.SWIPE_OUT_DELAY * this.numCols : 0;
+    var mult = direction === 'right' ? -1 : 1;
     $cols.each(function (i, el) {
       _.delay(function () {
-        $(el).addClass(self.outClass);
-      }, time -= self.SWIPE_OUT_DELAY);
+        $(el).addClass(self.outClass + '-' + direction);
+      }, time += self.SWIPE_OUT_DELAY * mult);
     });
 
     // Remove cols
@@ -87,9 +98,5 @@ $(function () {
   });
 
   $('#swipe-out').click(swipe.leave);
-
-  $('#effect').change(function (e) {
-    swipe.outClass = $(e.target).val();
-  });
 });
 
